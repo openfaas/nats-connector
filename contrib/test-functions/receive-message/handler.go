88315936762b04b3e4829570937f3f6518de09c9
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-02-21 20:36:09 +0000

    Update examples with timeouts
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- contrib/test-functions/receive-message/handler.go
+++ contrib/test-functions/receive-message/handler.go
@@ -2,15 +2,21 @@ package function
 
 import (
 	"fmt"
+	handler "github.com/openfaas-incubator/go-function-sdk"
 	"log"
 	"net/http"
-
-	handler "github.com/openfaas-incubator/go-function-sdk"
+	"os"
+	"time"
 )
 
 // Handle a serverless request
 func Handle(req handler.Request) (handler.Response, error) {
 	log.Printf("received %q", string(req.Body))
+
+	if val, ok := os.LookupEnv("wait"); ok && len(val) > 0 {
+		parsedVal, _ := time.ParseDuration(val)
+		time.Sleep(parsedVal)
+	}
 
 	return handler.Response{
 		Body:       []byte(fmt.Sprintf("Received msg: %q", string(req.Body))),
